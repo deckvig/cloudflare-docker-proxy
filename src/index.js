ea074d1d1ac6476749d1060bbeb592dceda3251b
Deckvig <2008.zhengxing@gmail.com>
2024-06-15 16:39:30 +0000

    update

--- src/index.js
+++ src/index.js
@@ -7,16 +7,16 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  "iregistry.your.us.kg": dockerHub,
+  "quay.iregistry.your.us.kg": "https://quay.io",
+  "gcr.iregistry.your.us.kg": "https://gcr.io",
+  "k8s-gcr.iregistry.your.us.kg": "https://k8s.gcr.io",
+  "k8s.iregistry.your.us.kg": "https://registry.k8s.io",
+  "ghcr.iregistry.your.us.kg": "https://ghcr.io",
+  "cloudsmith.iregistry.your.us.kg": "https://docker.cloudsmith.io",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.iregistry.your.us.kg": dockerHub,
 };
 
 function routeByHosts(host) {
